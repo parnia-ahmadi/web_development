d1345251a5cf30129435610dc655f9de88665f96
ezangeneh <zangeneh.erfan@aut.ac.ir>
2019-07-08 11:18:02 +0430

    use objects tutorial

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
 function showBulb() 
 {
     document.getElementById('myImage').style.display='block';
@@ -5,4 +14,23 @@ function showBulb()
 function hideBulb()
 {
     document.getElementById('myImage').style.display='none';
+}
+
+function func()
+{
+    person.firstName="ali";
+    person.lastName="hassani";
+    document.getElementById('p1').innerHTML = person.fullName();
+
+    // if(5 === "5") {
+    //     document.getElementById('p1').innerHTML = 1;
+    // }
+    // else {
+    //     document.getElementById('p1').innerHTML = 2;
+    // }
+    
+    // document.getElementById('p1').style="background-color:green;";
+    // // document.write("helllllllllllllllo");
+    // window.alert("hello")
+    // console.log("helllooo");
 }
